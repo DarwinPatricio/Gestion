c0154c7716ed1e5dd911f5b12d404a32581985eb
Patricio <darwinpatricio.hidrovo@fernando3martos.com>
2021-11-30 10:41:32 +0100

    creacion app t4

---
+++ gestionweb/lib/Pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina de Gestión'),
+        centerTitle: true,
+      ),
+      body: Wrap(
+        children: [],
+      ),
+    );
+  }
+}
